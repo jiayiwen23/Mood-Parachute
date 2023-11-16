9d8f9324121a7aa20a24e2084188c4c235bdd55e
dana <danazyt2016@gmail.com>
2023-11-15 21:00:31 -0800

    Working on the add entry screen

--- screens/AddEntryScreen.js
+++ screens/AddEntryScreen.js
@@ -10,20 +10,42 @@ export default function AddEntryScreen({ navigation }) {
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Date</Text>
       <TextInput
         style={styles.journal}
         value={text}
         onChangeText={setText}
         placeholder="Write down your thoughts"
       ></TextInput>
+      <View style={styles.image}>
+        <PressableButton>
+          <Text>Image</Text>
+        </PressableButton>
+        <PressableButton style={styles.image}>
+          <Text>submit</Text>
+        </PressableButton>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
   journal: {
+    flex: 3,
     backgroundColor: colors.white,
     width: "90%",
   },
+  text: {
+    flex: 1,
+    alignSelf: "flex-start",
+    padding: 10,
+    paddingLeft: 22,
+  },
+  image: { flex: 1 },
+  submit: { flex: 1 },
 });
