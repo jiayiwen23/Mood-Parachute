113db73254847c7256c237cb4affb10308fcacd0
dana <danazyt2016@gmail.com>
2023-11-15 20:06:59 -0800

    Implemented the firebase

--- components/EntriesList.js
+++ components/EntriesList.js
@@ -1,9 +1,29 @@
 import { View, ScrollView, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import EntryItem from "./EntryItem";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+import { database } from "../firebase/firebaseSetup";
+import { collection, onSnapshot } from "firebase/firestore";
 
 const EntriesList = ({ entries }) => {
-  console.log(entries);
+  const [journals, setJournals] = useState([]);
+  // Use onSnapshot to listen to realtime updates in Firestore
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      collection(database, "entries"),
+      (querySnapshot) => {
+        let newArray = [];
+        querySnapshot.docs.forEach((docSnap) => {
+          newArray.push({ ...docSnap.data(), id: docSnap.id });
+        });
+        setExpenses(newArray);
+      }
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
     <View>
       <ScrollView bounces={false} contentContainerStyle={styles.container}>
